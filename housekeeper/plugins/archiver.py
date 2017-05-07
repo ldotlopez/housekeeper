64e473fc844e9caf59562b0b2b07b8a426599696
Luis López <ldotlopez@gmail.com>
2017-05-07 12:55:57 +0200

    Archiver plugin

---
+++ housekeeper/plugins/archiver.py
@@ -0,0 +1,88 @@
+from housekeeper import pluginlib
+from housekeeper.lib import hkfilesystem, hkdatetime
+
+
+def archive(source, delta, destination=None, dry_run=False):
+    source = hkfilesystem.pathname_normalize(source)
+
+    if destination is None:
+        destination = source + ' (Archive)'
+    else:
+        destination = hkfilesystem.pathname_normalize(destination)
+
+    delta = hkdatetime.parse_timespan(delta)
+
+    ret = hkfilesystem.archive(
+        source,
+        destination,
+        diff=delta,
+        filter_func=is_archivable_filter,
+        dry_run=dry_run)
+
+    if dry_run:
+        for (op, *args) in ret:
+            if op == hkfilesystem.Operations.MOVE:
+                print("mv '{}' '{}'".format(*args))
+
+            elif op == hkfilesystem.Operations.UNLINK:
+                print("rm '{}'".format(*args))
+
+
+def is_archivable_filter(dirpath, dirnames, filenames):
+    # Skip .sync dir
+    try:
+        dirnames.remove('.sync')
+    except ValueError:
+        pass
+
+    # Skip hidden files
+    hidden = [x for x in filenames if x[0] == '.']
+    for x in hidden:
+        filenames.remove(x)
+
+
+class ArchiveCommand(pluginlib.Command):
+    __extension_name__ = 'archive'
+    HELP = 'Archive stuff'
+    ARGUMENTS = (
+        pluginlib.argument(
+            '-f', '--from',
+            dest='src',
+            required=True
+        ),
+        pluginlib.argument(
+            '-t', '--to',
+            dest='dst',
+            required=False
+        ),
+        pluginlib.argument(
+            '-d', '--delta',
+            required=True
+        ),
+        pluginlib.argument(
+            '-n', '--dry-run',
+            action='store_true',
+            required=False
+        )
+    )
+
+    def execute(self, app, arguments):
+        archive(source=arguments.src,
+                destination=arguments.dst,
+                delta=arguments.delta,
+                dry_run=arguments.dry_run)
+
+
+class CronTask(pluginlib.Task):
+    __extension_name__ = 'archive'
+    INTERVAL = '1H'
+
+    def execute(self, core):
+        for params in core.settings.get('archive', []):
+            archive(**params)
+
+
+__housekeeper_extensions__ = [
+    ArchiveCommand,
+    CronTask
+]
