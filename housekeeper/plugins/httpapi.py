4882149241600ccdd6fa8d5344d6136781aff9da
Luis López <ldotlopez@gmail.com>
2017-05-25 19:01:02 +0200

    Move API Server into core

--- housekeeper/plugins/httpapi.py
+++ housekeeper/plugins/httpapi.py
@@ -18,153 +18,26 @@
 # USA.
 
 
+from housekeeper import core
 from housekeeper import kit
 
 
-import json
-import mimetypes
-import multiprocessing
-import sys
+# import multiprocessing
 from os import path
 
 
-import falcon
 import gunicorn.app.base
 
 
-class JSONTranslator(object):
-    def process_request(self, req, resp):
-        # req.stream corresponds to the WSGI wsgi.input environ variable,
-        # and allows you to read bytes from the request body.
-        #
-        # See also: PEP 3333
-        if req.content_length in (None, 0):
-            # Nothing to do
-            return
-
-        body = req.stream.read()
-        if not body:
-            raise falcon.HTTPBadRequest('Empty request body',
-                                        'A valid JSON document is required.')
-
-        try:
-            req.context['doc'] = json.loads(body.decode('utf-8'))
-
-        except (ValueError, UnicodeDecodeError):
-            raise falcon.HTTPError(falcon.HTTP_753,
-                                   'Malformed JSON',
-                                   'Could not decode the request body. The '
-                                   'JSON was incorrect or not encoded as '
-                                   'UTF-8.')
-
-    def process_response(self, req, resp, resource):
-        if 'result' not in resp.context:
-            return
-
-        resp.body = json.dumps(resp.context['result'])
-
-
-class RequireJSON(object):
-    def process_request(self, req, resp):
-        if not req.client_accepts_json:
-            raise falcon.HTTPNotAcceptable(
-                'This API only supports responses encoded as JSON.',
-                href='http://docs.examples.com/api/json')
-
-        if req.method in ('POST', 'PUT'):
-            if 'application/json' not in req.content_type:
-                raise falcon.HTTPUnsupportedMediaType(
-                    'This API only supports requests encoded as JSON.',
-                    href='http://docs.examples.com/api/json')
-
-
-class MainResource:
-    def on_get(self, req, resp):
-        resp.status = falcon.HTTP_303
-        resp.location = '/static/index.html'
-
-
-class IntrospectionResource:
-    def __init__(self, reg, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.reg = reg
-
-    def on_get(self, req, resp):
-        resp.context['result'] = {
-            name: repr(ext)
-            for (name, ext)
-            in self.reg.items()
-        }
-
-
-class StaticSink:
-    def __init__(self, *args, prefix='/', **kwargs):
-        super().__init__(*args, **kwargs)
-        self.root = path.dirname(sys.modules['housekeeper'].__file__)
-        self.root = path.dirname(self.root)
-        self.prefix = prefix
-        self.mime = mimetypes.MimeTypes()
-
-    def on_get(self, req, resp):
-        filename = req.path
-        if not filename.startswith(self.prefix):
-            resp.status = falcon.HTTP_404
-            return
-
-        fullpath = path.realpath(self.root + filename)
-        if not fullpath.startswith(self.root):
-            resp.status = falcon.HTTP_404
-            return
-
-        try:
-            mime = self.mime.guess_type(fullpath)[0] or 'application/octet-stream'
-            resp.status = falcon.HTTP_200
-            resp.content_type = mime
-            with open(fullpath, 'r') as f:
-                resp.body = f.read()
-
-        except IOError:
-            resp.status = falcon.HTTP_404
-
-
-class APIServer(falcon.API):
-    def __init__(self, core, *args, **kwargs):
-        middleware = [RequireJSON(), JSONTranslator()]
-        super().__init__(*args, middleware=middleware, **kwargs)
-
-        self.registry = {}
-
-        for (name, ext) in core.get_extensions_for(kit.APIEndpoint):
-            self.setup_extension(name, ext)
-
-        self.add_route('/_/', IntrospectionResource(self.registry))
-
-    def setup_extension(self, name, ext):
-        path = '/' + name + '/'
-        self.add_route(path, ext)
-        self.registry[name] = ext
-        print("+ {} {}".format(path, ext))
-
-        for (name_, child) in ext.children.items():
-            self.setup_extension(name + '/' + name_, child)
-
-
-class UnicornApp(gunicorn.app.base.BaseApplication):
+class StandaloneApplication(gunicorn.app.base.BaseApplication):
     def __init__(self, app, options=None):
         self.options = options or {}
         self.application = app
-        sink = StaticSink().on_get
-
-        app.add_route('/', MainResource())
-        app.add_sink(sink, prefix='/static/')
         super().__init__()
 
     def load_config(self):
-        config = dict([
-            (key, value) for key, value in self.options.items()
-            if key in self.cfg.settings and value is not None
-        ])
-
+        config = dict([(key, value) for key, value in self.options.items()
+                       if key in self.cfg.settings and value is not None])
         for key, value in config.items():
             self.cfg.set(key.lower(), value)
 
@@ -176,7 +49,7 @@ class APIServerCommand(kit.Command):
     __extension_name__ = 'httpapi'
     HELP = 'Start HTTP API server'
 
-    def execute(self, app, arguments):
+    def execute(self, hk_app, arguments):
         options = {
             'bind': '127.0.0.1:8000',
             'graceful_timeout': 0,
@@ -186,7 +59,8 @@ class APIServerCommand(kit.Command):
             'timeout': 0,
             'workers': 1,  # (multiprocessing.cpu_count() * 2) + 1,
         }
-        server = UnicornApp(APIServer(app), options)
+
+        server = StandaloneApplication(core.APIServer(hk_app), options)
         server.run()
 
 
